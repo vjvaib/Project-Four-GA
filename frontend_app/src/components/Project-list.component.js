a75787f53995cf1bb3c49f7fc8ec5419b49c9369
vaibhav <joshi.vaibhav.j@gmail.com>
2020-02-22 12:41:52 +0800

    final updates

--- frontend_app/src/components/Project-list.component.js
+++ frontend_app/src/components/Project-list.component.js
@@ -1,76 +1,110 @@
-import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-const projectlist = props => (
-    <tr>
-        <td  className={props.projectlist.projectlist_completed ? 'completed' : ""}> {props.projectlist.projectlist_description}</td>
-        <td> className={props.projectlist.projectlist_completed ? 'completed' : ""}> {props.projectlist.projectlist_CRMNumber}</td>
-        <td> className={props.projectlist.projectlist_completed ? 'completed' : ""}> {props.projectlist.projectlist_sponsor}</td>
-        <td> className={props.projectlist.projectlist_completed ? 'completed' : ""}> {props.projectlist.projectlist_level}</td>
-        <td> className={props.projectlist.projectlist_completed ? 'completed' : ""}> {props.projectlist.projectlist_DueDate}</td>
+export default class ProjectList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { projectlists: [] };
+  }
 
-        <td>
-            <Link to={"/edit/"+props.projectlist._id}>Edit</Link>
-        </td>
-    </tr>
-)
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/projectlist/")
+      .then(response => {
+        console.log("response data", response.data);
+        this.setState({ projectlists: response.data });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
 
-export default class projectList extends Component {
+  componentDidUpdate() {
+    axios
+      .get("http://localhost:5000/projectlist/")
+      .then(response => {
+        this.setState({ projectlists: response.data });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {projectlists: []};
-    }
+  render() {
+    return (
+      <div>
+        <h3>Project List</h3>
+        <table className="table table-striped" style={{ marginTop: 20 }}>
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>CRM number</th>
+              <th>Sponsor</th>
+              <th>Level</th>
+              <th>Due Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.projectlists.map(function(currentprojectlist, i) {
+              return (
+                <tr>
+                  <td
+                    className={
+                      currentprojectlist.projectlist_completed
+                        ? "completed"
+                        : ""
+                    }
+                  >
+                    {currentprojectlist.projectlist_description}
+                  </td>
+                  <td
+                    className={
+                      currentprojectlist.projectlist_completed
+                        ? "completed"
+                        : ""
+                    }
+                  >
+                    {currentprojectlist.projectlist_CRMNumber}
+                  </td>
+                  <td
+                    className={
+                      currentprojectlist.projectlist_completed
+                        ? "completed"
+                        : ""
+                    }
+                  >
+                    {currentprojectlist.projectlist_sponsor}
+                  </td>
+                  <td
+                    className={
+                      currentprojectlist.projectlist_completed
+                        ? "completed"
+                        : ""
+                    }
+                  >
+                    {currentprojectlist.projectlist_level}
+                  </td>
+                  <td
+                    className={
+                      currentprojectlist.projectlist_completed
+                        ? "completed"
+                        : ""
+                    }
+                  >
+                    {currentprojectlist.projectlist_DueDate}
+                  </td>
 
-    componentDidMount() {
-        axios.get('http://localhost:5000/projectlist/')
-            .then(response => {
-                this.setState({projectlists: response.data});
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
-    }
-
-    componentDidUpdate() {
-      axios.get('http://localhost:5000/projectlist/')
-            .then(response => {
-                this.setState({projectlists: response.data});
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
-
-    }
-
-    projectList() {
-        return this.state.projectlists.map(function(currentprojectlist, i) {
-            return <projectlist projectlist={currentprojectlist} key={i} />;
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <h3>Project List</h3>
-                <table className="table table-striped" style={{ marginTop: 20 }}>
-                    <thead>
-                        <tr>
-                            <th>Description</th>
-                            <th>CRM number</th>
-                            <th>Sponsor</th>
-                            <th>Level</th>
-                            <th>Due Date</th>
-                            <th>Actions</th>
-
-                        </tr>
-                    </thead>
-                    <tbody>
-                        { this.projectList() }
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
+                  <td>
+                    <Link to={"/edit/" + currentprojectlist._id}>Edit</Link>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
--- frontend_app/src/components/edit-project.component.js
+++ frontend_app/src/components/edit-project.component.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 export default class EditProject extends Component {
-
   constructor(props) {
-
     super(props);
 
     this.onChangeprojectlist_description = this.onChangeprojectlist_description.bind(
@@ -23,125 +21,124 @@ export default class EditProject extends Component {
 
     this.state = {
       projectlist_description: "",
-      projectlist_CRMNumber : "",
-      projectlist_sponsor : "",
-      projectlist_Level : "",
-      projectlist_DueDate : "",
-      projectlist_completed : false
-    }
+      projectlist_CRMNumber: "",
+      projectlist_sponsor: "",
+      projectlist_Level: "",
+      projectlist_DueDate: "",
+      projectlist_completed: false
+    };
   }
 
-  componentDidMount () {
-    axios.get('http://localhost:5000/projectlist/' + this.props.match.params.id)
-   .then (response => {
-     this.setState ({
-       projectlist_description :response.data.projectlist_description,
-       projectlist_CRMNumber :response.data.projectlist_CRMNumber,
-       projectlist_sponsor: response.data.projectlist_sponsor,
-       projectlist_Level: response.data.projectlist_Level,
-       projectlist_DueDate: response.data.projectlist_DueDate,
-       projectlist_completed :response.data.projectlist_completed
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/projectlist/" + this.props.match.params.id)
+      .then(response => {
+        this.setState({
+          projectlist_description: response.data.projectlist_description,
+          projectlist_CRMNumber: response.data.projectlist_CRMNumber,
+          projectlist_sponsor: response.data.projectlist_sponsor,
+          projectlist_Level: response.data.projectlist_Level,
+          projectlist_DueDate: response.data.projectlist_DueDate,
+          projectlist_completed: response.data.projectlist_completed
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
 
-     })
-   }) 
-
-   .catch(function(error) {
-
-    console.log(error);
-    
-   })
-
-   onChangeprojectlist_description(e) {
+  onChangeprojectlist_description(e) {
     this.setState({
       projectlist_description: e.target.value
     });
   }
 
- onChangeprojectlistCRMNumber(e) {
- this.setState ({
-
-projectlist_CRMNumber: e.target.value
-
- });
-
- }
-
- onChangeprojectlist_DueDate(e) {
-  this.setState({
-    projectlist_DueDate: e.target.value
-  });
-}
-
-onChangeprojectlist_Completed(e) {
-  this.setState({
-    projectlist_completed:!this.state.projectlist_completed
-  });
-}
-
-
-onSubmit (e) {
-
-  e.preventDefault();
-  const obj = {
-    projectlist_description: this.state.projectlist_description,
-    projectlist_CRMNumber: this.state.projectlist_CRMNumber,
-    projectlist_completed: this.state.projectlist_completed,
-  };
-
-  axios.post('http://localhost:5000/projectlist/update/' + this.props.match.params.id, obj)
-
-  .then(res => console.log(res.data));
-
-  this.props.history.push('/');
-  
-}
-
+  onChangeprojectlist_CRMNumber(e) {
+    this.setState({
+      projectlist_CRMNumber: e.target.value
+    });
   }
+
+  onChangeprojectlist_DueDate(e) {
+    this.setState({
+      projectlist_DueDate: e.target.value
+    });
+  }
+
+  onChangeprojectlist_completed(e) {
+    this.setState({
+      projectlist_completed: !this.state.projectlist_completed
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const obj = {
+      projectlist_description: this.state.projectlist_description,
+      projectlist_CRMNumber: this.state.projectlist_CRMNumber,
+      projectlist_completed: this.state.projectlist_completed
+    };
+
+    axios
+      .post(
+        "http://localhost:5000/projectlist/update/" +
+          this.props.match.params.id,
+        obj
+      )
+
+      .then(res => console.log(res.data));
+
+    this.props.history.push("/");
+  }
+
   render() {
     return (
       <div>
         <h3> Update projectlist</h3>
         <form onSubmit={this.onSubmit}>
-<div className="form-group">
-  <label> Description: </label>
-  <input type="text"
-  className="form-control"
-  value={this.state.projectlist_description}
-  onChange={this.onChangeprojectlistdescription}
-  />
-</div>
+          <div className="form-group">
+            <label> Description: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.projectlist_description}
+              onChange={this.onChangeprojectlist_description}
+            />
+          </div>
 
-<div className="form-group">
-  <label> CRMNumber: </label>
-  <input type="text"
-  className="form-control"
-  value={this.state.projectlist_CRMNumber}
-  onChange={this.onChangeprojectlistCRMNumber}
-  />
-</div>
-<div className ="form-check">
+          <div className="form-group">
+            <label> CRMNumber: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.projectlist_CRMNumber}
+              onChange={this.onChangeprojectlist_CRMNumber}
+            />
+          </div>
+          <div className="form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="completedCheckbox"
+              name="completedCheckbox"
+              onChange={this.onChangeprojectlist_completed}
+              checked={this.state.projectlist_completed}
+              value={this.state.projectlist_completed}
+            />
 
-  <input type="checkbox"
-  className ="form-check-input"
-  id ="completedCheckbox"
-  name="completedCheckbox"
-  onChange={this.onChangeprojectlistCompleted}
-  checked ={this.state.projectlist_completed}
-  value ={this.state.projectlist_completed}
-  />
-
-  <label className="form-check-label" htmlFor="completedCheckBox">
-
-    Completed
-  </label>
-</div>
-<br/>
-<div className="form-group">
-
-<input type ="submit" value = "Update projectlist" className="btn btn-primary" />
-</div>
+            <label className="form-check-label" htmlFor="completedCheckBox">
+              Completed
+            </label>
+          </div>
+          <br />
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Update projectlist"
+              className="btn btn-primary"
+            />
+          </div>
         </form>
-      
       </div>
     );
   }
